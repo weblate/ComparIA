9a5ec8f6a89361b93e2155f5501fefbdb0295860
Hadrien <ketsapiwiq@protonmail.com>
2025-07-23 18:52:49 +0200

    wip: dsfr

--- frontend/src/lib/components/FAQContent.svelte
+++ frontend/src/lib/components/FAQContent.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   // import { m } from '$lib/i18n/messages'
   // import { externalLinkProps, sanitize } from '$lib/utils/commons'
-  import "@gouvfr/dsfr/dist/component/accordion/accordion.module.min.js?client"
+  // import "@gouvfr/dsfr/dist/component/accordion/accordion.module.min.js?client"
   export const prerender = false;
 </script>
 
